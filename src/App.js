9fcb8acf82811bb913a1847e8be2ca6179f4027b
sklit-dsk <danilasklovskii.dev@gmail.com>
2024-12-10 18:27:43 +0100

    upd scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="wrapper clear">
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="Logo pic"/>
+          <img width={40} height={40} src="/img/logo.svg" alt="Logo pic"/>
           <div>
             <h3 className="text-uppercase">Perfume market</h3>
             <p className="opacity-5">The most delicious perfume</p>
@@ -11,18 +11,27 @@ function App() {
         </div>
         <ul className="d-flex">
           <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.png" alt="Cart pic"/>
+            <img width={18} height={18} src="/img/cart.svg" alt="Cart pic"/>
             <span>50 eur</span>
           </li>
           <li>
-            <img width={20} height={20} src="/img/user.png" alt="User pic"/>
+            <img width={20} height={20} src="/img/user.svg" alt="User pic"/>
           </li>
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">All perfume</h1>
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>All perfume</h1>
+          <div className="search-block d-flex"> 
+            <img src="/img/search.svg" alt = "Search"/>
+            <input type="text" placeholder="Search..."/>
+          </div>
+        </div>
+
+
         <div className="d-flex">
           <div className="card">
+            <img src="/img/heart-unliked.svg" alt ="Heart unliked"/>
             <img width={133} height={112} src="/img/perfume/1.png" alt="Perfume"/>
             <h5>Women's perfume Chanel</h5>
             <div className="d-flex justify-between align-center">
@@ -31,7 +40,7 @@ function App() {
                 <b>99 eur</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="/img/add.png" alt="Add" />
+                <img width={11} height={11} src="/img/btn-add.svg" alt="Add" />
               </button>
             </div>
           </div>
@@ -44,7 +53,7 @@ function App() {
                 <b>99 eur</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="/img/add.png" alt="Add" />
+                <img width={11} height={11} src="/img/btn-add.svg" alt="Add" />
               </button>
             </div>
           </div>
@@ -57,7 +66,7 @@ function App() {
                 <b>99 eur</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="/img/add.png" alt="Add" />
+                <img width={11} height={11} src="/img/btn-add.svg" alt="Add" />
               </button>
             </div>
           </div>
@@ -70,7 +79,7 @@ function App() {
                 <b>99 eur</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="/img/add.png" alt="Add" />
+                <img width={11} height={11} src="/img/btn-add.svg" alt="Add" />
               </button>
             </div>
           </div>
