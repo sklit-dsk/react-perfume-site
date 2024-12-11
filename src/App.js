ab1cca2908dce5442f278e435370dccd3388eda2
sklit-dsk <danilasklovskii.dev@gmail.com>
2024-12-11 20:13:44 +0100

    layout finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="d-flex mb-30 justify-between ">Cart <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove button" /></h2>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{backgroundImage: 'url(/img/perfume/1.png)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Women's perfume Chanel</p>
+                <b>50 eur</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove button" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{backgroundImage: 'url(/img/perfume/1.png)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Women's perfume Chanel</p>
+                <b>50 eur</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove button" />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{backgroundImage: 'url(/img/perfume/1.png)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Women's perfume Chanel</p>
+                <b>50 eur</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove button" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>150 eur</b>
+              </li>
+              <li>
+                <span>Fee 5%:</span>
+                <div></div>
+                <b>142,5 eur</b>
+              </li>
+            </ul>
+            <button className="greenButton">Place an order <img src="/img/arrow.svg" alt="Arrow"/></button>
+          </div>
+          
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.svg" alt="Logo pic"/>
@@ -31,7 +78,9 @@ function App() {
 
         <div className="d-flex">
           <div className="card">
-            <img src="/img/heart-unliked.svg" alt ="Heart unliked"/>
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt ="Heart unliked"/>
+            </div>
             <img width={133} height={112} src="/img/perfume/1.png" alt="Perfume"/>
             <h5>Women's perfume Chanel</h5>
             <div className="d-flex justify-between align-center">
