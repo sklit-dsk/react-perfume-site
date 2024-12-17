88dc59da85121ee24c3a8f7a5f24ca7bb12e6b16
sklit-dsk <danilasklovskii.dev@gmail.com>
2024-12-17 19:01:34 +0100

    upd imgs path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ function Header(props) {
         <header style={styles} className="d-flex justify-between align-center p-40">
             <div className="d-flex align-center">
                 <Link to="/">
-                    <img width={40} height={40} src="/img/logo.svg" alt="Logo pic" />
+                    <img width={40} height={40} src="img/logo.svg" alt="Logo pic" />
                 </Link>
                 <div>
                     <h3 className="text-uppercase">Perfume market</h3>
@@ -19,17 +19,17 @@ function Header(props) {
             </div>
             <ul className="d-flex">
                 <li className="mr-20 cu-p" onClick={props.onClickCart}>
-                    <img width={18} height={18} src="/img/cart.svg" alt="Cart pic" />
+                    <img width={18} height={18} src="img/cart.svg" alt="Cart pic" />
                     <span>{totalPrice} eur</span>
                 </li>
                 <li className="mr-20 cu-p">
                     <Link to="/favorites">
-                        <img width={20} height={20} src="/img/heart.svg" alt="Heart pic" />
+                        <img width={20} height={20} src="img/heart.svg" alt="Heart pic" />
                     </Link>
                 </li>
                 <li className="mr-5 cu-p">
                     <Link to="/orders">
-                        <img width={22} height={22} src="/img/user.svg" alt="User pic" />
+                        <img width={22} height={22} src="img/user.svg" alt="User pic" />
                     </Link>
                 </li>
             </ul>
